2dec54dccdc6f1af0b7595c7b238d4341fdec1db
Pranav Deshmukh <52973587+pranav918@users.noreply.github.com>
2020-08-10 17:43:59 +0530

    Add files via upload

---
+++ Bit Manipulation/Flip_ith_bit.cpp
@@ -0,0 +1,15 @@
+/* Author : Pranav Deshmukh
+            PICT,Pune
+   Stay Focused!
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int n, i;
+	cout << "Enter number followed by ith(from 0) bit place:" << endl;
+	n = n ^ (1 << i);
+	cout << "Number after flipping is:" << n << endl;
+	return 0;
+}
